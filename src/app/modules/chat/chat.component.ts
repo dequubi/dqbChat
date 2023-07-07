1f3019ce0bb2e4d3435b41d5f5104bf1b868ad7b
Alexey Zezin <alexey.zezin@gmail.com>
2023-07-07 11:28:45 +0300

    WIP chat display

--- src/app/modules/chat/chat.component.ts
+++ src/app/modules/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, Renderer2, RendererFactory2 } from '@angular/core';
 import { TmiService } from '@app/modules/chat/services/tmi.service';
 
 @Component({
@@ -15,15 +15,27 @@ export class ChatComponent implements OnInit {
 
     public messageList = this.tmiService.messageList;
 
-    constructor(private tmiService: TmiService) {
+
+    constructor (private tmiService: TmiService,
+                 private renderer: Renderer2
+    ) {
     }
 
-    ngOnInit(): void {
+    ngOnInit (): void {
         this.newMessage$.subscribe({
             next: (msg) => {
-                console.log(msg.userState.username, msg.message);
+                console.log(msg);
             }
         });
+    }
+
+    test (msg: HTMLDivElement): void {
+        // msg.className = 'chat__message';
+        // console.log(msg.offsetHeight);
+        setTimeout(() => {
+            this.renderer.setStyle(msg, 'display', 'block');
+            this.renderer.setStyle(msg, 'margin-bottom', '0');
+        }, 300);
     }
 
 }
