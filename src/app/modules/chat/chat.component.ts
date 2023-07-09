81bbe177eb78b3e68fa2a4c72fcaf131e51b89b4
Alexey Zezin <alexey.zezin@gmail.com>
2023-07-09 18:25:25 +0300

    Chat display and animation

--- src/app/modules/chat/chat.component.ts
+++ src/app/modules/chat/chat.component.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, Component, OnInit, Renderer2, RendererFactory2 } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { TmiService } from '@app/modules/chat/services/tmi.service';
+import anime from 'animejs/lib/anime.es';
 
 @Component({
     selector: 'app-chat',
@@ -10,18 +11,19 @@ import { TmiService } from '@app/modules/chat/services/tmi.service';
 })
 export class ChatComponent implements OnInit {
 
+    @ViewChild('container') msgContainer: ElementRef<HTMLDivElement>;
+
     public newMessage$ = this.tmiService.newMessage$.asObservable();
     public messageList$ = this.tmiService.messageList$.asObservable();
 
     public messageList = this.tmiService.messageList;
 
-
-    constructor (private tmiService: TmiService,
-                 private renderer: Renderer2
+    constructor(private tmiService: TmiService,
+                private renderer: Renderer2
     ) {
     }
 
-    ngOnInit (): void {
+    ngOnInit(): void {
         this.newMessage$.subscribe({
             next: (msg) => {
                 console.log(msg);
@@ -29,13 +31,24 @@ export class ChatComponent implements OnInit {
         });
     }
 
-    test (msg: HTMLDivElement): void {
-        // msg.className = 'chat__message';
-        // console.log(msg.offsetHeight);
-        setTimeout(() => {
-            this.renderer.setStyle(msg, 'display', 'block');
-            this.renderer.setStyle(msg, 'margin-bottom', '0');
-        }, 300);
+    messageInit(msg: HTMLDivElement): void {
+        this.animateMessageAppearance(msg);
     }
 
+    private animateMessageAppearance(msg: HTMLDivElement, duration = 500): void {
+        msg.style.minWidth = `${this.msgContainer.nativeElement.offsetWidth - 8}px`;
+        msg.style.maxWidth = `${this.msgContainer.nativeElement.offsetWidth - 8}px`;
+        console.log(msg.offsetWidth, this.msgContainer.nativeElement.offsetWidth);
+        const height = msg.offsetHeight;
+        msg.style.minWidth = 'initial';
+        msg.style.maxWidth = 'initial';
+        msg.style.position = 'initial';
+        anime({
+            targets: msg,
+            height: ['0', height - 8],
+            duration
+        }).finished.then(() => {
+            // msg.style.height = 'initial';
+        });
+    }
 }
